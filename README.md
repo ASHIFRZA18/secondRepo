578d27470663bd4ea343588834a17d4f532ba56c
ASHIF <ashifrza18@gmail.com>
2025-11-07 20:15:20 +0530

    Add new repo

---
+++ README.md
@@ -0,0 +1,4 @@
+#My Small intro
+
+I'm trying to create second repo on github with my small intro
+#thankyou #keepLearning
---
+++ app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button")
+btn.addEventListener("click",function sayThank() {
+    alert("THANK-YOU FOR READING😍")
+})
+
+sayThank();
+
+
